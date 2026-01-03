45e7a7e8eb151407f49b5d863b839fa80a483dc1
yonal5 <yonalabasingha@gmail.com>
2026-01-03 20:54:32 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLIC_KEY
-)
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
